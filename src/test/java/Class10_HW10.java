1c75d4e25bc4a517787094657ee4bb3241bd0cd5
michaelkhasin <khasin13@gmail.com>
2020-09-08 22:29:40 +0300

    Adding exercises text in main

--- src/test/java/Class10_HW10.java
+++ src/test/java/Class10_HW10.java
@@ -1,4 +1,100 @@
+/**
+ Class 10
+ Write a program that does the following:
+ 1. Write a TestNG test that will open the following:
+ a. Chrome – https://www.walla.co.il
+ b. FireFox – https://www.ynet.co.il
 
+ 2. In one of the browsers do the following:
+ a. Create a String variable with the website’s title
+ b. Refresh website
+ c. Get website title and use assertion to test if website’s
+ name is equal to the String variable you created in clause 1.
+
+ 3. Create a test that assert** Website URL
+ ** Prepare strings variable containing the title and URL before
+
+ 4.
+  Open Chrome browser.
+  Navigate to Google Translate website:
+ https://translate.google.com/
+  Find translation box (where you enter text to be
+ translated) using 2 different locators and print the
+ WebElement you created.
+ e.g: System.out.println(driver.findElement…)
+
+ 5.
+  Open FireFox browser.
+  Navigate to youtube: https://www.youtube.com/
+  Find Youtube button using id locator and print the
+ WebElement you created.
+ e.g. System.out.println(driver.findElement…)
+
+
+ 6.
+  Open FireFox browser.
+  Navigate to selenium website:
+ http://www.seleniumhq.org/
+  Find the search text field using name locator and print
+ the WebElement you created.
+ e.g. System.out.println(driver.findElement…)
+  Send the word “selenium” to this text box
+
+ 7.
+  Open Chrome browser on Amazon website:
+ https://www.amazon.com/
+  Assert website title.
+  Search for “Leather shoes”.
+
+ 8.
+ Create a TestNG test with the following:
+  Open Google Translate web page
+  Write anything in Hebrew in the text area
+
+ 9.
+  Open Youtube web page
+  Type a name of a song (any)
+  Click on search.
+
+ 10.
+  Open https://dgotlieb.github.io/Controllers/
+  Select “Cheese option” in radio buttons.
+  Select one of the options from the drop down below.
+  Print all elements in drop down.
+
+
+ 11.
+  Open Chrome browser on Facebook website
+ https://www.facebook.com/
+  Login into Facebook (No need to send me credentials).
+
+
+ 12.
+ Create a TestNG test with the following:
+  Enter https://dgotlieb.github.io/WebCalculator/
+  Print “7” button dimensions.
+  Check if “6” button is displayed
+  Prepare a String variable with any number
+  Calculate a mathematical formula that will give the
+ result you choose in the String variable earlier.
+  Use assert to check if you got the expected result
+ ** For example: press 4  press + button  press 2  Press = button
+ The above will generate the result 6 (4+2).
+
+ 13.
+ Create a TestNG test with the following:
+  Enter https://dgotlieb.github.io/WebCalculator/
+  Print “2” button height.
+  Print “6” button width.
+
+ 14.
+  Find a way to disable all
+ extensions in: Chrome
+ browser
+  Run browsers without
+ extensions.
+
+ */
 
 public class Class10_HW10 {
     public static void main(String[] args) {
